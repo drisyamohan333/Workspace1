5cf548fd50a99fa18c5964ee0ec1e3f786c18fb0
drisya <drisyamohan333@gmail.com>
2017-11-30 16:08:40 +0530

    Added XmlDemo.

---
+++ XmlDemo/Controllers/ProductCategoryController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XmlDemo.Models;
+
+namespace XmlDemo.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        private ProductDbContext db = new ProductDbContext();
+
+        //
+        // GET: /ProductCategory/
+
+        public ActionResult Index()
+        {
+            return View(db.ProductCategories.ToList());
+        }
+
+        //
+        // GET: /ProductCategory/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            ProductCategory productcategory = db.ProductCategories.Find(id);
+            if (productcategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productcategory);
+        }
+
+        //
+        // GET: /ProductCategory/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /ProductCategory/Create
+
+        [HttpPost]
+        public ActionResult Create(ProductCategory productcategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductCategories.Add(productcategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productcategory);
+        }
+
+        //
+        // GET: /ProductCategory/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            ProductCategory productcategory = db.ProductCategories.Find(id);
+            if (productcategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productcategory);
+        }
+
+        //
+        // POST: /ProductCategory/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productcategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productcategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productcategory);
+        }
+
+        //
+        // GET: /ProductCategory/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            ProductCategory productcategory = db.ProductCategories.Find(id);
+            if (productcategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productcategory);
+        }
+
+        //
+        // POST: /ProductCategory/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductCategory productcategory = db.ProductCategories.Find(id);
+            db.ProductCategories.Remove(productcategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ XmlDemo/Models/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XmlDemo.Models
+{
+    public class ProductCategory
+    {
+
+        public int ProductCategoryID { get; set; }
+        [System.ComponentModel.DataAnnotations.Display(Name = "Category")]
+        public string strCategory { get; set; }
+        //Navigational property
+        public virtual ICollection<Product> products { get; set; }
+    }
+}
